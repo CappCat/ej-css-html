dd1a8b4f3056bf8823ededf3aacd372d10ee46be
CappCat <abiudvera123@gmail.com>
2024-09-03 20:46:11 -0500

    innitializing repo

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('1').addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/drdanielvera?igsh=YmViNWRtdmswZDl3';
+    });
+
+    document.getElementById('2').addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/anaschnabelstudio?igsh=dTdrZTN0ZGxoNW43';
+    });
+
+    document.getElementById('3').addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/sushigo_chone?igsh=MmVjdjZsbWU2ZTJw';
+    });
+
+    document.getElementById('4').addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/cromchone?igsh=eXR6dWMyMzA0Y2Q4';
+    });
+
+    document.getElementById('5').addEventListener('click', function() {
+        window.location.href = 'https://www.instagram.com/porsupollo_chone?igsh=ZTZnN3g1ZG5oOGgw';
+    });
+});
